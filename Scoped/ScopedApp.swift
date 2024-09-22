c3ff2084f02aab8dea3addae404f0ef1ad91559d
Mason Dierkes <mjdierkes@msn.com>
2024-09-22 18:05:27 -0500

    Initial Commit

---
+++ Scoped/ScopedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScopedApp.swift
+//  Scoped
+//
+//  Created by Mason Dierkes on 9/22/24.
+//
+
+import SwiftUI
+
+@main
+struct ScopedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
